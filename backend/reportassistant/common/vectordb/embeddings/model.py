c054e6015deeebfcc7a5c89c83f00686951ee4e6
Hegedüs Botond <71768180+hbotond1999@users.noreply.github.com>
2024-10-10 21:04:22 +0200

    milvus

---
+++ backend/reportassistant/common/vectordb/embeddings/model.py
@@ -0,0 +1,46 @@
+from milvus_model.hybrid.bge_m3 import BGEM3EmbeddingFunction
+
+from reportassistant.common.vectordb.embeddings.schema import EmbeddingsResult
+
+
+from typing import List
+
+class BgeM3EmbeddingsModel:
+    """
+    A class to interact with the BGE M3 embedding model, providing methods to generate
+    sparse and dense vectors from input content and retrieve the dimensionality of dense vectors.
+    """
+
+    @classmethod
+    def get_model(cls):
+        """
+        Get the BGE M3 embedding model instance. This model is cached for repeated use.
+
+        Returns:
+            BGEM3EmbeddingFunction: The embedding model initialized with the "BAAI/bge-m3" configuration.
+        """
+        return BGEM3EmbeddingFunction(model_name="BAAI/bge-m3")
+
+    @classmethod
+    def create_sparse_dense_vectors(cls, contents: List[str]) -> EmbeddingsResult:
+        """
+        Generates sparse and dense embedding vectors from the given list of documents or text.
+
+        Args:
+            contents (List[str]): A list of text documents to encode into sparse and dense vectors.
+
+        Returns:
+            EmbeddingsResult: An object containing the resulting sparse and dense vectors.
+        """
+        result = cls.get_model().encode_documents(contents)
+        return EmbeddingsResult(sparse_vectors=result["sparse"], dense_vectors=result["dense"])
+
+    @classmethod
+    def get_dense_dim(cls) -> int:
+        """
+        Get the dimensionality of the dense vectors produced by the BGE M3 model.
+
+        Returns:
+            int: The dimensionality of the dense vectors.
+        """
+        return cls.get_model().dim["dense"]
