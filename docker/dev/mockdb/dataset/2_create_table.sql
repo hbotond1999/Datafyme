194e05c345ee4cd7c30897ad29605d51a43f228c
suhajda.net <you@example.com>
2024-10-14 23:05:26 +0200

    adding mock database to docker init

---
+++ docker/dev/mockdb/dataset/2_create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE fulfillment.deliveries (
+	delivery_id int4 NULL,
+	delivery_date varchar(50) NULL,
+	product_id varchar(50) NULL,
+	location_id varchar(50) NULL,
+	quantity int4 NULL
+);
