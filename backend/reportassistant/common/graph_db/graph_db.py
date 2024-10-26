1a0df187db8660e4a11417be66a3558236600983
suhajda.net <you@example.com>
2024-10-26 20:24:02 +0200

    graph db helper functions

--- backend/reportassistant/common/graph_db/graph_db.py
+++ backend/reportassistant/common/graph_db/graph_db.py
@@ -32,12 +32,13 @@ class Neo4JInstance:
     def _create_and_return_relation(self, table1, table2):
 
         query = (
-            "CREATE (t1:Table { name: $table1 }) "
-            "CREATE (t2:Table { name: $table2 }) "
-            "CREATE (t1)-[:RELATED_TO]->(t2) "
-            "CREATE (t2)-[:RELATED_TO]->(t1) "
+            "MERGE (t1:Table { name: $table1 }) "
+            "MERGE (t2:Table { name: $table2 }) "
+            "MERGE (t1)-[:RELATED_TO]->(t2) "
+            "MERGE (t2)-[:RELATED_TO]->(t1) "
             "RETURN t1.name, t2.name"
         )
+
         try:
             record = self.driver.execute_query(query, table1=table1, table2=table2, database_=self.database,
                                                result_transformer_=lambda r: r.single(strict=True))
@@ -47,14 +48,19 @@ class Neo4JInstance:
             logging.error("%s raised an error: \n%s", query, exception)
             raise
 
-    def find_table_neighbours(self, table):
-        names = self._find_and_return_table_neighbours(table)
-        for name in names:
-            print(f"Neighbour of {table}: {name}")
+    def clear_graph_database(self):
+        query = "MATCH (n) DETACH DELETE n"
+        try:
+            self.driver.execute_query(query, database_=self.database)
+            logging.info("Az egész gráf adatbázis kiürítve.")
 
-    def _find_and_return_table_neighbours(self, table):
+        except (DriverError, Neo4jError) as exception:
+            logging.error("Hiba történt az adatbázis kiürítésekor: %s", exception)
+            raise
+
+    def find_table_neighbours(self, table):
         query = (
-            "MATCH (t:Table)-[]-(neighbour) "
+            "MATCH (t:Table)-[]->(neighbour) "
             "WHERE t.name = $table "
             "RETURN neighbour.name AS name"
         )
@@ -63,35 +69,3 @@ class Neo4JInstance:
                                                routing_=RoutingControl.READ,
                                                result_transformer_=lambda r: r.value("name"))
         return neighbours
-
-
-if __name__ == "__main__":
-    neo4j_instance = Neo4JInstance()
-
-    # get postgres_relations:
-    response = requests.get('http://127.0.0.1:8000/dbloader')
-
-    if response.status_code == 200:
-        data = response.json()
-        relations = data.get('relations', [])
-        table_foreign_pairs = []
-        for relation in relations:
-            if relation is not None:
-                table_name = relation.get("table_name")
-                foreign_table_name = relation.get("foreign_table_name")
-
-                if table_name and foreign_table_name:
-                    table_foreign_pairs.append({"table_name": table_name, "foreign_table_name": foreign_table_name})
-
-        print(table_foreign_pairs)
-    else:
-        table_foreign_pairs = []
-        print(f"DJANGO API ERROR: {response.status_code}")
-
-    try:
-        for table_pair in table_foreign_pairs:
-            neo4j_instance.create_relation(table_pair["table_name"], table_pair["foreign_table_name"])
-
-        neo4j_instance.find_table_neighbours(table_foreign_pairs[0]["table_name"])
-    finally:
-        neo4j_instance.close()
