c2fe800b5f7aecd599eb975bd9cdad3440fe19ef
suhajda.net <you@example.com>
2024-10-26 20:24:37 +0200

    creating Django endpoints to graph db

--- backend/reportassistant/dbloader/urls.py
+++ backend/reportassistant/dbloader/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.loader, name="loader"),
+    path('relation_graph/create', views.create_relation_graph, name="create_relation_graph"),
+    path('relation_graph/clear', views.clear_relation_graph, name="clear_relation_graph"),
+    path('relation_graph/get_neighbours', views.get_neighbours, name="get_neighbours"),
 ]
