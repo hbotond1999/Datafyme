09bb356fe2ab3e7850dff9e2251d914f5c0b640a
Hegedüs Botond <botond.hegedus@barre.hu>
2024-11-13 16:33:44 +0100

    fix typo

--- backend/reportassistant/reporter_agent/visualisation_agent/chart/histogram.py
+++ backend/reportassistant/reporter_agent/visualisation_agent/chart/histogram.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Field
 
 
 class HistogramChart(BaseModel):
-    y_axis_column: str = Field(description="he Y-axis displays the count or frequency of values falling within each bin. The height of each bar represents the number of data points within that bin’s range.")
+    y_axis_column: str = Field(description="The Y-axis displays the count or frequency of values falling within each bin. The height of each bar represents the number of data points within that bin’s range.")
