9205356749643977a6c72f18fd465585c3287daa
suhajda.net <you@example.com>
2024-11-16 22:28:57 +0100

    adding sql_agent endpoint

--- backend/reportassistant/reporter_agent/views.py
+++ backend/reportassistant/reporter_agent/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+from reporter_agent.sql_statement_creator.ai.graph import create_sql_agent_graph
+
 
 # Create your views here.
+def sql_agent(request):
+    message = request.GET.get('message')
+
+    sql_graph = create_sql_agent_graph()
+    matching_tables = sql_graph.invoke({"message": message})
+    print(matching_tables)
+
+    return JsonResponse(data={"result": matching_tables}, safe=False)
