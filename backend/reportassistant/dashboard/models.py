f1d462b6ac6ae38e39a1a3ee4497a2dace0a3b44
Hegedüs Botond <71768180+hbotond1999@users.noreply.github.com>
2024-11-17 16:39:26 +0100

    Update models.py

--- backend/reportassistant/dashboard/models.py
+++ backend/reportassistant/dashboard/models.py
@@ -9,12 +9,12 @@ class Dashboard(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2500)
-    user_id = models.IntegerField(get_user_model())
+    user = models.IntegerField(get_user_model())
 
 
 class DashboardSlot(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
-    dashboard_id = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
-    chart_id = models.OneToOneField(Chart, on_delete=models.CASCADE)
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
+    chart = models.OneToOneField(Chart, on_delete=models.CASCADE)
     row_num = models.IntegerField()
     col_num = models.IntegerField()
