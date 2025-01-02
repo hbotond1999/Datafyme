2c239ffbecd68635f10caac0f5b7df7172cf3de1
suhajda.net <you@example.com>
2025-01-02 13:04:32 +0100

    bugfix: solving movie_id inconsistency

--- docker/dev/mockdb/datasets/02_create_table_actors.sql
+++ docker/dev/mockdb/datasets/02_create_table_actors.sql
@@ -2,7 +2,7 @@
 CREATE TABLE movies_metadata.actors (
 	id varchar(50) NULL,
 	"name" varchar(50) NULL,
-	description varchar(50) NULL,
 	birthplace varchar(50) NULL,
-	birth_of_date varchar(50) NULL
+	birth_of_date varchar(50) NULL,
+    description varchar(500) NULL
 );
